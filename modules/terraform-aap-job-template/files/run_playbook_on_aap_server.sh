262809ecb5416d79338275e37fc1ace209954ed2
Chris Stella <christopher.stella@hashicorp.com>
2025-05-14 20:07:06 +0000

    satisfied variable inputs for ansible-playbook command

--- modules/terraform-aap-job-template/files/run_playbook_on_aap_server.sh
+++ modules/terraform-aap-job-template/files/run_playbook_on_aap_server.sh
@@ -10,4 +10,4 @@ tar --strip-components=4 -xvf \
 # install ansible-controller collection locally for us
 ansible-galaxy collection install ansible-controller-4.6.11.tar.gz
 
-ansible-playbook configure_aap_job_template.yml
+#ansible-playbook -i 'localhost,' -c local -e \"inventory_name_for_jt=${inventory_name_for_jt} aap_organization_name=${aap_organization_name}\" aap_controller_host=${aap_controller_host} aap_controller_username=${aap_controller_username} aap_controller_password=${aap_controller_password} aap_validate_certs=${aap_validate_certs} project_name_for_playbook=${project_name_for_playbook} machine_credential_name=${machine_credential_name} machine_credential_private_key=${machine_credential_private_key} machine_credential_username=${machine_credential_username} job_template_name_from_playbook=${job_template_name_from_playbook} machine_credential_name=${machine_credential_name} configure_aap_job_template.yml
--- modules/terraform-aap-job-template/main.tf
+++ modules/terraform-aap-job-template/main.tf
@@ -12,18 +12,22 @@ resource "terraform_data" "run_playbook_on_aap_server" {
 
   connection {
     type        = "ssh"
-    user        = "ec2-user"
+    user        = "cstella"
     private_key = var.aap_server_ssh_private_key
     host        = local.aap_controller_ip
   }
 
   provisioner "file" {
-    source      = "configure_aap_job_template.yml"
+    source      = "${path.module}/files/configure_aap_job_template.yml"
     destination = "configure_aap_job_template.yml"
   }
 
   provisioner "remote-exec" {
-    script = "run_playbook_on_aap_server.sh"
+    script = "${path.module}/files/run_playbook_on_aap_server.sh"
+  }
+
+  provisioner "remote-exec" {
+    inline = [ "ansible-playbook -i 'localhost,' -c local -e \"inventory_name_for_jt='${var.inventory_name_tf}' aap_organization_name='${var.aap_organization_name}' aap_controller_host='${var.aap_controller_url}' aap_controller_username='${var.aap_controller_username}' aap_controller_password='${var.aap_controller_password}' aap_validate_certs='${var.aap_validate_certs}' project_name_for_playbook='${var.project_name_for_playbook}' machine_credential_name='${var.machine_credential_name}' machine_credential_private_key='${var.machine_credential_private_key}' machine_credential_username='${var.machine_credential_username}' job_template_name_from_playbook='${var.job_template_name_from_playbook}' machine_credential_name='${var.machine_credential_name}'\" configure_aap_job_template.yml" ]
   }
 }
 
